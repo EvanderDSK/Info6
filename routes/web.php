d82893f9266a640f608d5c4b8823423d4918b860
EvanderDSK <evanderdsk@comunidad.unam.mx>
2022-04-19 21:14:30 -0500

    Cambios clase 9

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 
 
 Route::resource('/post', PostController::class ) ;
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
